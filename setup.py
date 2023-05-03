db958a443eb7a94eeba8c2bea308e610faca981b
Mkreitman <120761192+Mkreitman@users.noreply.github.com>
2023-05-03 14:12:06 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project focuses on College Scorecard data provided by the U.S. Department of Education. The purpose it to explore student data to find useful insights on increasing graduation rates. In particular, I'd like to see if there is statistical significance between ACT/SAT scores at admittance and graduation rate after 4 years of enrollment.',
+    author='Maxwell Kreitman',
+    license='',
+)
